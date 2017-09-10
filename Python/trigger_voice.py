eced5998644d984ec6778427c76f5e3d904dc980
masa8224 <pankrit.krit@gmail.com>
2017-09-10 11:47:24 +0700

    some changes

--- Python/trigger_voice.py
+++ Python/trigger_voice.py
@@ -1,16 +1,39 @@
 import RPi.GPIO as GPIO
 import subprocess
 import time
+import pygame
+file_ryoukai = 'sound/ryoukai.mp3'
+file_hai = 'sound/hai.mp3'
+file_cmdinvalid = 'sound/invalidcmd.mp3'
+file_init = 'sound/init.mp3'
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
 GPIO.setup(17, GPIO.IN)
 GPIO.setup(18, GPIO.OUT)
+pygame.init()
+pygame.mixer.init()
+def playsound(file):
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy(): 
+        pygame.time.Clock().tick(10)
+def blinkled(pin,n,delay):
+    x = 0
+    while x < n:
+        GPIO.output(pin, GPIO.HIGH)
+        time.sleep(delay)
+        GPIO.output(pin, GPIO.LOW)
+        if n > 1:
+            time.sleep(delay)
+        x = x+1
+playsound(file_init)
 while True:
     while not GPIO.input(17):
         time.sleep(0.01)
     print "Listening..."
     CURRENT_STATE = 1
+    playsound(file_hai)
     GPIO.output(4,GPIO.HIGH)
     proc = subprocess.Popen("voicecommand",stdout=subprocess.PIPE)
     
@@ -19,18 +42,12 @@ while True:
       if line != '':        
         if line.rstrip() == "No translation" or line.rstrip() == "Could not find answer. Try again.":
             print "Invalid command"
-            GPIO.output(18, GPIO.HIGH)
-            time.sleep(0.25)
-            GPIO.output(18, GPIO.LOW)
-            time.sleep(0.25)
-            GPIO.output(18, GPIO.HIGH)
-            time.sleep(0.25)
-            GPIO.output(18, GPIO.LOW)
+            playsound(file_cmdinvalid)
+            blinkled(18,2,0.25)
         else:
-            print line.rstrip()
-            GPIO.output(18, GPIO.HIGH)
-            time.sleep(0.5)
-            GPIO.output(18, GPIO.LOW)
+            print line.rstrip()            
+            playsound(file_ryoukai)
+            blinkled(18,1,1)
       else:
         break
     GPIO.output(4,GPIO.LOW) 
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # MeguHome
 It's just home automation system 
+
 but with Megumi sound!
+
+This project utilize PiAUISuites voicecommand as voice recognizer
